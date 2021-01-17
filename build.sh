a54287c5e1d92843269809408acfc9683ba0f599
vczyh <zhangeek@qq.com>
2021-01-17 19:00:50 +0800

    添加编译版本

--- build.sh
+++ build.sh
@@ -3,4 +3,16 @@
 # Linux
 export GOOS=linux
 export GOARCH=amd64
-go build -o build/v2rayT
+go build -o releases/v2rayT-linux-amd64
+
+
+# Mac
+export GOOS="darwin"
+export GOARCH="amd64"
+go build -o releases/v2rayT-darwin-amd64
+
+# Mac ARM
+export GOOS="darwin"
+export GOARCH="arm64"
+go build -o releases/v2rayT-darwin-arm64
+
